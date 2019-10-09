7b0f78befc03c64bbc64f46ebb0191b41910088e
Carla Vieira <carlaamvieira@gmail.com>
2019-10-09 16:04:39 -0300

    Paginas office hours

--- office_hours/urls.py
+++ office_hours/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('', views.check, name='check'),
     path('checkin/', views.checkin, name='checkin'),
     path('checkout/', views.checkout, name='checkout'),
-    path('history/', views.history, name='office_hours_history'),
+    path('myhistory/', views.myhistory, name='office_hours_history'),
+    path('officehour_list/', views.officehour_list, name='officehour_list'),
+    path('edit/<int:id>', views.officehour_update, name='officehour_edit'),
+    path('delete/<int:id>', views.officehour_delete, name='officehour_delete'),
 
 ]
--- office_hours/views.py
+++ office_hours/views.py
@@ -1,18 +1,18 @@
 
-from datetime import datetime, date, timedelta
-
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 
+from office_hours.forms import OfficeHourForm
 from office_hours.models import OfficeHour
 
-
+@login_required
 def check(request):
     timenow = timezone.now()
     return render(request, 'office_hours/check.html', {'request_user': request.user, 'timenow': timenow})
 
-
+@login_required
 def checkin(request):
     if request.user.is_working:
         return HttpResponse("Você já está fazendo office hour :)")
@@ -24,7 +24,7 @@ def checkin(request):
         return redirect('office_hours_history')
     return render(request, {'request_user': request.user})
 
-
+@login_required
 def checkout(request):
     if request.user.is_working:
         officehour = OfficeHour.objects.get(member=request.user, checkout_time__isnull=True)
@@ -36,9 +36,32 @@ def checkout(request):
         return redirect('office_hours_history')
     else:
         return HttpResponse("Você precisa fazer check in antes :)")
-    return render(request, 'office_hours/history.html', {'request_user': request.user})
+    return render(request, 'office_hours/myhistory.html', {'request_user': request.user})
 
-
-def history(request):
+@login_required
+def myhistory(request):
     office_hours = OfficeHour.objects.filter(member=request.user)
-    return render(request, 'office_hours/history.html', {'office_hours': office_hours, 'request_user': request.user})
+    return render(request, 'office_hours/myhistory.html', {'office_hours': office_hours, 'request_user': request.user})
+
+@login_required
+def officehour_list(request):
+    office_hours = OfficeHour.objects.all()
+    return render(request, 'office_hours/officehour_list.html', {'office_hours': office_hours, 'request_user': request.user})
+
+@login_required
+def officehour_update(request, id):
+    officehour = OfficeHour.objects.get(id=id)
+    form = OfficeHourForm(request.POST or None, instance=OfficeHour)
+    if form.is_valid():
+        form.save()
+        return redirect('officehours_list')
+    return render(request, 'office_hours/officehour_update.html', {'form': form, 'officehour': officehour, 'request_user': request.user})
+
+@login_required
+def officehour_delete(request, id):
+    officehour = OfficeHour.objects.get(id=id)
+    if request.method == 'POST':
+        officehour.delete()
+        return redirect('officehours_list')
+    return render(request, 'office_hours/officehour_confirm_delete.html', {'officehour': officehour, 'request_user': request.user})
+
