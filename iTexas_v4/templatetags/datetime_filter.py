2ac85dc871a5b0d80a8b907c3e27e9743be5bfa2
oGuimaraes <otaviovgsr@gmail.com>
2019-10-30 02:35:02 -0300

    linha clicavel tabelas

--- iTexas_v4/templatetags/datetime_filter.py
+++ iTexas_v4/templatetags/datetime_filter.py
@@ -15,7 +15,7 @@ def format_datetime(value):
     if value is not None:
         hours, rem = divmod(value.seconds, 3600)
         minutes, seconds = divmod(rem, 60)
-        return '{:02d}:{:02d}'.format(hours, minutes)
+        return '{:02d}:{:02d}:{:02d}'.format(hours, minutes, seconds)
     else:
         return '-'
 
